37bfec20e1ae13e215f704655ea9075a8d25d189
masaboe <masaboe@gmail.com>
2022-03-09 10:06:08 +0700

    Materi minggu 1 dan 2 pwl

---
+++ minggu-2/coba.php
@@ -0,0 +1,20 @@
+<?php
+		$a="Hall0";
+	?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	
+	<form>
+		Nilai a: <input type="text" name="a"><br>
+		Nilai b: <input type="text" name="b"><br>
+		
+	</form>
+	<?php
+		echo $a;
+	?>
+</body>
+</html>
